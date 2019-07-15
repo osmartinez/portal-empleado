67552fb41eda99c087967ccb2a8547a45355f0db
oscar <osmartin@unirioja.es>
2019-07-15 23:40:54 +0200

    Añade ficheros para testing e2e

---
+++ src/tests/e2eTests/config.test.js
@@ -0,0 +1,17 @@
+exports.config = {
+    tests: './*_test.js',    // how to know which files are test files
+    output: './output',      // where to save screenshots
+    helpers: {
+     WebDriverIO: {               // which backend helper to use
+       url: 'http://app:4000',    // a base URL to start on
+       host: 'firefox-container', // identifying where selenium runs
+       browser: 'firefox',        // a series of config options
+       smartWait: 5000,              
+       waitForTimeout: 10000,
+       desiredCapabilities: {        // for a demo app we do not want 
+           acceptInsecureCerts: true,   //to worry about SSL certs
+       }
+     },
+    },
+    name: 'codeceptjs-docker',
+  };
