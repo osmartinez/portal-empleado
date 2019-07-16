7710053c49ead2ddc36e1af8c9719483b6745989
oscar <osmartin@unirioja.es>
2019-07-16 19:42:12 +0200

    Añade test e2e

--- src/tests/e2eTests/basic.test.js
+++ src/tests/e2eTests/basic.test.js
@@ -3,4 +3,8 @@ Feature('Basic test');
 Scenario('navigate to homepage', I => {
   I.amOnPage('/');
   I.see('Acceder');
+  I.fillField("username","omartinez")
+  I.fillField("password","Hackerox4")
+  I.click('Acceder')
+  I.see("Portal web","h1")
 });
