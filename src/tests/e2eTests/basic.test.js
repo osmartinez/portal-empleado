a0fcb0ffc080c891442edaf2ff715ecb9efb619a
oscar <oscarmtzz22@gmail.com>
2019-07-16 14:09:59 +0200

    Cambia dockerfile.e2e

--- src/tests/e2eTests/basic.test.js
+++ src/tests/e2eTests/basic.test.js
@@ -1,7 +1,7 @@
 Feature('Basic test');
 
 Scenario('navigate to homepage', I => {
-  I.amOnPage('http://app:3000');
+  I.amOnPage('http://app:4001');
   I.saveScreenshot('frontpageScreenshot.png');
   I.see('Hello');
 });
