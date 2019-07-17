28a3197e8a2c6c6ba2ae05088b0999820d91a142
oscar <oscarmtzz22@gmail.com>
2019-07-17 07:19:21 +0200

    Cambia puerto aplicación

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const app = express()
 require('./lib/passport')
 
 // configuracion
-app.set('port',process.env.PORT || 4000)
+app.set('port',process.argv[2]||4000)
 app.set('views',path.join(__dirname,'views'))
 app.engine('.hbs',exphbs({
     defaultLayout: 'main',
