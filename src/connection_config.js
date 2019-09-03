a21b5462bba6500e4059152f90b9f3459d07879a
oscar <oscarmtzz22@gmail.com>
2019-09-03 12:47:52 +0200

    Cambio config db connection

--- src/connection_config.js
+++ src/connection_config.js
@@ -2,15 +2,15 @@ const config =
 {
     authentication: {
         options: {
-            userName: 'oscar', // update me
-            password: 'Oeseceaerre2' // update me
+            userName: 'oscar_user_db', // update me
+            password: '@@112233Aa' // update me
         },
         type: 'default'
     },
-    server: 'portal-empleado.database.windows.net', // update me
+    server: 'somos-arneplant.database.windows.net', // update me
     options:
     {
-        database: 'portal_empleado', //update me
+        database: 'somos-arneplant', //update me
         encrypt: true,
         rowCollectionOnRequestCompletion: true,
         useColumnNames: true // For easier JSON formatting
